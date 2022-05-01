8159089ec5a0b11eaa24a6c9153ae6f8d50ebcb8
marleigh <volcana@gmail.com>
2022-04-30 17:00:23 -0700

    it all works! only CSS changes at this point

--- src/Main.js
+++ src/Main.js
@@ -11,13 +11,14 @@ import useTopping from "./hooks/useTopping";
 import Topping from "./components/Topping";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleDown, faAngleLeft} from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 
 export default function Main() {
     const [dropdownState, setDropdownState] = useState({
         theme: ""
     });
     const [displayTopping, setDisplayTopping] = useState(false);
+    const [toggleTopping, setToggleTopping] = useState(true);
 
     const {getPrompt, chosenPrompts} = useGenerator(dropdownState);
     const {getTopping, newTopping} = useTopping();
@@ -36,6 +37,7 @@ export default function Main() {
 
     function handleIconClick() {
         //set state and hide this section! also change icon
+        setToggleTopping(prevState => !prevState)
     }
 
     return (
@@ -56,23 +58,28 @@ export default function Main() {
             </section>
 
             {displayTopping &&
-            <section className="max-w-2xl flex-col justify-center m-auto bg-gray-green-light p-6 my-2 shadow-sm shadow-gray-dark/30">
-                <FontAwesomeIcon 
-                    icon={faAngleLeft} 
-                    onClick={handleIconClick}
-                    className="text-3xl text-dark-green hover:text-gray-dark"
-                />
-                <section className="flex justify-center items-baseline my-2">
-                    <h3 className="m-4">
-                        Need a topping to go with that flavor?
-                    </h3>
-                    <Button 
-                        handleClick={getTopping}
-                        buttonName={"Get Topping"}
+                <section className="max-w-2xl grid-cols-2 grid-rows-3 m-auto bg-gray-green-light p-6 my-2 shadow-sm shadow-gray-dark/30">
+                    <FontAwesomeIcon 
+                        icon={toggleTopping ? faAngleDown : faAngleLeft}
+                        onClick={handleIconClick}
+                        className="text-3xl text-dark-green hover:text-gray-dark items-end col-span-3"
                     />
+                    {toggleTopping &&
+                        <section className="my-2 col-span-1">
+                            <div className="flex justify-center">
+                                <h3 className="m-4">
+                                    Need a topping to go with that flavor?
+                                </h3>
+                                <Button 
+                                    handleClick={getTopping}
+                                    buttonName={"Get Topping"}
+                                />
+                            </div>
+
+                            {newTopping}
+                        </section>
+                    }
                 </section>
-                    {newTopping}
-            </section>
             }
 
             <section className="flex justify-between flex-wrap">
--- src/components/Topping.js
+++ src/components/Topping.js
@@ -1,6 +1,6 @@
 export default function Topping(props) {
     return (
-        <div className="p-4 bg-gray/30 rounded-md text-center">
+        <div className="p-4 bg-gray/30 rounded-md text-center col-span-3">
            <h4 className="text-lg font-bold">
                 Topping: &nbsp;
                 <span className="text-gray-dark">{props.name}</span>
